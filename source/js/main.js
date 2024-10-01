5e001fe1e83687c69d66d05bed43c3bf8f678579
CrashStatic <thecrashstatic@gmail.com>
2024-10-01 11:43:21 +0300

    Удаляет лишний код

--- source/js/main.js
+++ source/js/main.js
@@ -1,12 +1,6 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
 import { initMenu } from './modules/menu';
 import { initSwiperHero } from './modules/slider/hero-slider';
 import { initModal } from './modules/modal';
-// import { setCustomSelect } from './modules/form/custom-select';
-// import { initForm } from './modules/form/validate-form';
 import { initSwiperPrograms } from './modules/slider/programs-slider';
 import { initSwiperNews } from './modules/slider/news-slider';
 import { initFaqAccordion } from './modules/faq';
@@ -16,8 +10,6 @@ import { initMainForm } from './modules/form-main';
 initMenu();
 initSwiperHero();
 initModal();
-// setCustomSelect();
-// initForm();
 initSwiperPrograms();
 initSwiperNews();
 initFaqAccordion();
