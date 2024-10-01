3b977f204527a79ad7c01588be3d9dbb3d616705
CrashStatic <thecrashstatic@gmail.com>
2024-10-01 11:42:19 +0300

    Удаляет лишний код

--- source/js/modules/form/custom-select.js
+++ source/js/modules/form/custom-select.js
@@ -1,16 +1,12 @@
 const selectElement = document.getElementById('place');
 const fragment = document.createDocumentFragment();
 const label = document.querySelector('.form__label');
-// const labelSelect = label.cloneNode(true);
-// labelSelect.textContent = 'Выберите город';
 
 const setCustomSelect = (inputClass, labelClass, container) => {
   const customSelect = document.createElement('input');
   customSelect.setAttribute('class', `${inputClass}`);
   customSelect.setAttribute('tabindex', '0');
   customSelect.setAttribute('readonly', 'true');
-  // customSelect.setAttribute('required', '');
-  // customSelect.setAttribute('value', '');
 
   customSelect.setAttribute('required', 'true');
 
@@ -22,9 +18,6 @@ const setCustomSelect = (inputClass, labelClass, container) => {
 
   for (let i = 0; i < selectElement.length; i++) {
     const optionDiv = document.createElement('div');
-    // optionDiv.setAttribute('required', 'true');
-
-    // optionDiv.setAttribute('class', 'form__div');
 
     optionDiv.setAttribute('tabindex', '0');
     optionDiv.innerHTML = selectElement.options[i].innerHTML;
@@ -32,7 +25,6 @@ const setCustomSelect = (inputClass, labelClass, container) => {
       selectElement.selectedIndex = i;
       customSelect.innerHTML = this.innerHTML;
       customSelect.value = this.innerHTML;
-      // customSelect.setAttribute('data-value', `${this.innerHTML}`);
       closeAllSelect();
     });
 
@@ -51,31 +43,14 @@ const setCustomSelect = (inputClass, labelClass, container) => {
 
   customSelect.addEventListener('click', (e) => {
     e.stopPropagation();
-    // closeAllSelect(this);
     optionsContainer.classList.toggle('form__select-items--opened');
   });
 
-  // const isEnterKey = (evt) => evt.key === 'Enter';
-
   customSelect.addEventListener('keydown', (e) => {
-    // e.stopPropagation();
-    // e.preventDefault();
-
     if (e.key === 'Enter') {
       e.preventDefault();
       optionsContainer.classList.toggle('form__select-items--opened');
-
-      // for (let i = 0; i < selectElement.length; i++) {
-      //   const div = document.querySelector('.form__div');
-      //   div.setAttribute('tabindex', '0');
-
-      // }
-      // optionDiv.setAttribute('tabindex', '0');
-
     }
-    // closeAllSelect(this);
-    // onSelectKeydown();
-    // optionsContainer.classList.toggle('select-items--opened');
   });
 
   const labelSelect = label.cloneNode(true);
@@ -87,14 +62,6 @@ const setCustomSelect = (inputClass, labelClass, container) => {
   fragment.appendChild(labelSelect);
   container.appendChild(fragment);
 
-  // const isEnterKey = (evt) => evt.key === 'Enter';
-
-  // function onSelectKeydown() {
-  //   if (isEnterKey) {
-  //     optionsContainer.classList.toggle('select-items--opened');
-  //   }
-  // }
-
   function closeAllSelect(elmnt) {
     const items = document.getElementsByClassName('form__select-items');
     for (let i = 0; i < items.length; i++) {
@@ -105,21 +72,6 @@ const setCustomSelect = (inputClass, labelClass, container) => {
   }
 
   document.addEventListener('click', closeAllSelect);
-
-
-  // document.querySelector('.form__input--place').addEventListener('click', function() {
-  //   this.nextElementSibling.classList.toggle('show');
-  // });
-
-  // document.querySelectorAll('.form__select-items div').forEach((option) => {
-  //   option.addEventListener('click', function() {
-  //     const select = document.getElementById('place');
-  //     select.value = this.getAttribute('data-value');
-  //     document.querySelector('.form__input--place').textContent = this.textContent;
-  //     // this.parentNode.classList.remove('show');
-  //   });
-  // });
-
 };
 
 export { setCustomSelect };
